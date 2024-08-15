859b178dd326762f269864db1b4f75b23693a732
OkiljonMuhammad <maxmadmurodovoqiljon@gmail.com>
2024-08-15 08:50:43 +0500

    The first version of API is completed

---
+++ backend/todo-api/app/Policies/TodoPolicy.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Todo;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class TodoPolicy
+{
+   
+    public function modify(User $user, Todo $post): Response
+    {
+        return $user->id === $post->user_id
+            ? Response::allow()
+            : Response::deny("Bu eslatmani o'chirolmaysiz");
+    }
+}
